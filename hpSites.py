176ccc9e0894901316b7659346a598dcbbd7fa74
MeetMithlesh <89780425+MeetMithlesh@users.noreply.github.com>
2025-06-26 15:22:15 +0530

    Update hpSites.py

--- hpSites.py
+++ hpSites.py
@@ -8,3 +8,9 @@ async def read_hp_sites():
     with open("hpSites.json", "r") as f:
         data = json.load(f)
     return data
+
+@router.get("/corebridge-sites")
+async def read_corebridge_sites():
+    with open("coreBridgeSites.json", "r") as f:
+        data = json.load(f)
+    return data
